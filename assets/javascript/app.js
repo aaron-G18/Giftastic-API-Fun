2ee6517bf5a3c7dabe9b3e2732b17ada5e7149bf
Aaron Garner <aaron.garner.1982@gmail.com>
2020-02-28 14:41:39 -0800

    got the top premade buttons to pull gifs and populate the correct area of the page. Need to work on the create your own button logic and the logic to stop/start animation on the gifs.

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+$(".top-box").on("click", ".search-button", function () {
+    $(".content-container").empty();
+    var search = $(this).attr("data-text");
+    var queryURL =
+        "https://api.giphy.com/v1/gifs/search?q=" +
+        search +
+        "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var gifImage = $("<img>");
+            gifDiv.attr("class", "thumbnail");
+            gifImage.attr("src", results[i].images.fixed_height_small.url);
+            gifDiv.append(p);
+            gifDiv.append(gifImage);
+            $(".content-container").append(gifDiv);
+        }
+    });
+});
